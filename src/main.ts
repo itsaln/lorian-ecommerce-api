d470e5dd3027b859b2ff6e0710355a2f33e60e0b
itsaln <itsaln23@gmail.com>
2023-04-13 11:05:21 +0500

    01

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from '@app/app.module'
-import { PrismaService } from '@app/prisma.service'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, { cors: true })
 
-	const prismaService = app.get(PrismaService)
-	await prismaService.enableShutdownHooks(app)
+	// const config = new DocumentBuilder()
+	// 	.setTitle('Itsaln API')
+	// 	.setDescription('The api of my portfolio project')
+	// 	.setVersion('1.0')
+	// 	.build()
+
+	// const document = SwaggerModule.createDocument(app, config)
+	// SwaggerModule.setup('api', app, document)
 
 	app.setGlobalPrefix('api')
 	await app.listen(process.env.PORT || 5000)
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
+import { ProductController } from '@app/product/product.controller'
 import { PrismaService } from '@app/prisma.service'
 import { ReviewService } from '@app/review/review.service'
-import { ProductController } from '@app/product/product.controller'
 import { ProductService } from '@app/product/product.service'
 
 @Module({
